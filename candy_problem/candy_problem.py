8ba6dae2d1c85204d078146bc678fef231c3fb1a
Mikelle Wade <mikelle.wade@gadadevelopersacademy.org>
2024-09-16 14:06:12 -0400

    updated README and removed directions from candy file

--- candy_problem/candy_problem.py
+++ candy_problem/candy_problem.py
@@ -1,15 +1,3 @@
-'''
-1. 
-In `get_friends_favorite_candy_count()`, return a dictionary of candy names 
-and the amount of times each candy appears in the `friend_favorites` list.
-
-friend_favorites = [
-    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
-    ["Bob", ["milky way", "licorice", "lollipop" ]],
-    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
-    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
-]
-'''
 def get_friends_favorite_candy_count(favorites):
     candy_dict = {}
 
@@ -19,19 +7,6 @@ def get_friends_favorite_candy_count(favorites):
     
     return candy_dict
 
-'''
-2. 
-Given the list `friend_favorites`, create a new data structure in the 
-function `create_new_candy_data_structure` that describes the different 
-kinds of candy paired with a list of friends that like that candy. 
-
-friend_favorites = [
-    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
-    ["Bob", ["milky way", "licorice", "lollipop" ]],
-    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
-    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
-]
-'''
 def create_new_candy_data_structure(data):
     candy_friend_dictionary = {}
 
@@ -47,28 +22,12 @@ def create_new_candy_data_structure(data):
     
     return candy_friend_dictionary
 
-'''
-3. 
-In `get_friends_who_like_specified_candy()`, return a tuple of 
-friends who like the candy specified in the candy_name parameter.
-'''
 def get_friends_who_like_specific_candy(data, candy_name):
     candy_dict = create_new_candy_data_structure(data)
 
     return tuple(candy_dict[candy_name])
 
-'''
-4. 
-In, `create_candy_set()`, return a set of all the candies from
-the data structure made in `create_new_candy_data_structure()`.
-'''
 def create_candy_set(data):
     return set(data.keys())
 
-'''
-5. 
-Starting with nominal cases, write tests for each of the functions 
-in the file tests/test_candy_data_structure.py then write tests to 
-handle edge cases.
-'''
 
