f12e7ec13ec2ffe4c6672a3206bca3201baffa57
Tatyana Ageyeva <tatyana.ageyeva.v@gmail.com>
2025-03-18 17:34:32 -0700

    My implementation and new test for check

--- candy_problem/candy_problem.py
+++ candy_problem/candy_problem.py
@@ -1,13 +1,60 @@
+friend_favorites = [
+    ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
+    [ "Bob", ["milky way", "licorice", "lollipop"]],
+    [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+    [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+]
+    # create a dict of candy in the friend_favorites 
+    # add key name of a candy value number of times said candy appears in friend_favorites
+    # count each candy appears in the friend_favorites list
+    # return  dict
 def get_friends_favorite_candy_count(favorites):
-    pass
+    candy_count_dict = {}
 
-def create_new_candy_data_structure(data):
-    pass 
+    for friend_candy_faforites in favorites:
+    #["Sally", [ "lollipop", "bubble gum", "laffy taffy"]]
+        for candy in friend_candy_faforites[1]:
+            if candy in candy_count_dict:
+                candy_count_dict[candy] += 1
+            else:
+                candy_count_dict[candy] = 1
+    print (candy_count_dict)
+    return candy_count_dict
 
-def get_friends_who_like_specific_candy(data, candy_name):
-    pass
+#get_friends_favorite_candy_count(friend_favorites)
 
-def create_candy_set(data):
-    pass 
+# create a dict with keys for specific candy with value of 
+#  friends who like it
+#create_new_candy_data_structure_dict =
+#{
+#     "lollipop": ["Sally","Bob"],
+#     "bubble gum": ["Sally"],
+#     ....
+#}
+#
+def create_new_candy_data_structure(favorites):
+    favorite_candy = {}
+    for friend_candies in favorites:
+        for candy in friend_candies[1]:
+            if candy not in favorite_candy:
+                favorite_candy[candy] = [friend_candies[0]]
+            else:
+                favorite_candy[candy].append(friend_candies[0])
+    return  favorite_candy
+
+create_new_candy_data_structure(friend_favorites)
+
+
+def get_friends_who_like_specific_candy(favorites, candy_name):
+    candy_friends_dict = create_new_candy_data_structure(favorites)
+    value = candy_friends_dict.get(candy_name, [])
+    return tuple(value)
+#print (get_friends_who_like_specific_candy(friend_favorites,"lollipop"))
+
+
+def create_candy_set(favorites):
+    candy_friends_dict = create_new_candy_data_structure(favorites)
+    return (set(candy_friends_dict.keys()))
+#print (create_candy_set(friend_favorites))
 
 
--- tests/test_candy_data_structure.py
+++ tests/test_candy_data_structure.py
@@ -43,3 +43,71 @@ Starting with nominal cases, write tests for each of the functions
 in the file tests/test_candy_data_structure.py then write tests to 
 handle edge cases.
 '''
+# Add your own assertions to the test below
+def test_get_friends_who_like_specific_candy():
+
+    # Arrange
+    friend_favorites = [
+        ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+
+    # Act
+    result = get_friends_who_like_specific_candy(friend_favorites, "lollipop")
+    
+    # Assert
+    assert result == ('Sally', 'Bob')
+# Edge: 
+# pass candy does not exist in a friends favorite
+def test_get_candy_not_in_friends_favorite():
+
+    # Arrange
+    friend_favorites = [
+        ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+
+    # Act
+    result = get_friends_who_like_specific_candy(friend_favorites, "lollipop_test")
+    
+    # Assert
+    assert result == ()
+
+def test_create_candy_set():
+
+    # Arrange
+    friend_favorites = [
+        ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+
+    # Act
+    result = create_candy_set(friend_favorites)
+    
+    # Assert
+    #assert result == {'sour patch kids', 'chocolate bar', 'licorice', 'lollipop', 'bubble gum', 'nerds', 'laffy taffy', 'milky way'}
+    # check if result is set
+    assert type(result) == set
+
+def test_create_candy_set_length():
+
+    # Arrange
+    friend_favorites = [
+        ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
+        [ "Bob", ["milky way", "licorice", "lollipop"]],
+        [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
+        [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
+    ]
+
+    # Act
+    result = create_candy_set(friend_favorites)
+    
+    # Assert
+    # check result length
+    assert len(result) == 8
